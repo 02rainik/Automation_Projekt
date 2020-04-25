44137b9c774509fd283a36e6db327a7e461d5b6c
ritschinator <61366360+ritschinator@users.noreply.github.com>
2020-04-25 15:37:59 +0200

    Update Program.cs

--- Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
+++ Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
@@ -7,60 +7,63 @@ namespace Zahlensysteme_Gruppe_C
 {
     class Program
     {
-        static string Eingabezahl,Eingabemodus;
         /// <summary>
         /// Zahlensysteme_Gruppe_C
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Titel");
-            Console.WriteLine("Bitte Geben Sie die Binärzahl ein");         
-            Eingabezahl = Console.ReadLine();
-            Console.WriteLine("In welches Zahlensystem wollen sie ihre Zahl konvertieren?(Hex/Dec)");
-            Eingabemodus = Console.ReadLine();
-
             Console.ReadKey();
         }
 
         /// <summary>
         /// 0 = Nix / 1 = Hex / 2 = Dec
         /// </summary>
-        static int HexorBin = 4;
+        static int HexorDec = 4;
         /// <summary>
         /// Auswahl zwischen Hex ind Bin
         /// </summary>
         /// <param name="Eingabe">Benuzer Eingabe</param>
         static void Switch(string Auswahl, string BinZahl)
         {
+            bool BoolHexorDec = false;
             for (int i = 0; i < BinZahl.Length; i++)
 			{
                 if (Convert.ToInt32(BinZahl.Substring(i, 1)) == 0 || Convert.ToInt32(BinZahl.Substring(i, 1)) == 1)
                 {
-                    switch (Auswahl)
-                    {
-                        case "Hex":
-                            HexorBin = 1;
-                            break;
-
-                        case "Dec":
-                            HexorBin = 2;
-                            break;
-
-                        default:
-                            HexorBin = 0;
-                            Console.WriteLine("Fehlerhafte Eingabe");
-                            break;
-                    }
+                    BoolHexorDec = true;
                 }
                 else
                 {
-                    Console.WriteLine("Eingabe ist keine Binärzahl");
+                    BoolHexorDec = false;
                 }
 			}
+            if (BoolHexorDec)
+            {
+                switch (Auswahl)
+                {
+                    case "Hex":
+                        HexorDec = 1;
+                        break;
 
+                    case "Dec":
+                        HexorDec = 2;
+                        break;
 
+                    default:
+                        HexorDec = 0;
+                        Console.WriteLine("Fehlerhafte Eingabe");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Eingabe ist keine Binärzahl");
+            }
+            
 
+            
         }
     }
 }
+
